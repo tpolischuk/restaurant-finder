6c1f66e9dbd09ac1a769f78f34979f92acac8e4a
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-18 12:25:29 -0700

    Octicons, mobile layout for detail view and back function

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import {
     NavbarBrand
 } from 'reactstrap';
 
-export default class Example extends React.Component {
+import Octicon, { ChevronLeft } from '@primer/octicons-react'
+
+export default class Header extends React.Component {
     constructor(props) {
         super(props);
 
@@ -13,16 +15,28 @@ export default class Example extends React.Component {
             isOpen: false
         };
     }
+
     toggle() {
+
+        //For if we need a toggling nav in the future
         this.setState({
             isOpen: !this.state.isOpen
         });
     }
+
+    handleBackButton() {
+        let restaurantDetail = document.querySelector('.restaurant-detail');
+        restaurantDetail.classList.remove('transitioned');
+        restaurantDetail.classList.add('transitioning');
+    }
+
     render() {
         return (
             <div>
                 <Navbar light expand="md" className="justify-content-between fixed-top">
-                    <span className=""></span>
+                    <span className="back-span" onClick={this.handleBackButton}>
+                        <Octicon className="back-button" icon={ChevronLeft} />
+                    </span>
                     <NavbarBrand href="/" className="text-center">Lunch Tyme</NavbarBrand>
                     <img alt="map" className="map-icon" src="/images/icon_map@2x.png" />
                 </Navbar>
